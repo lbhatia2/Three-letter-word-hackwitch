bb68a1c82d1c60c0b56e51c7521d6f3cce9a8188
Lina Bhatia <lina@bhatia.org>
2022-07-22 09:49:36 -0500

    Initial Commit

---
+++ Three letter word hackwitch/Three_letter_word_hackwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_letter_word_hackwitchApp.swift
+//  Three letter word hackwitch
+//
+//  Created by Lina Bhatia on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Three_letter_word_hackwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
